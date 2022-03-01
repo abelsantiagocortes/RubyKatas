8f5f9ad9ce291f3d1940055cbd68708c15a334e2
Abel Cortes <abelsantiagocortes@tirant.com>
2022-03-01 18:30:27 +0100

    Kata String Calculator

--- ruby/implementation.rb
+++ ruby/implementation.rb
@@ -1,5 +1,25 @@
-class Kata
-  def self.example
-    true
+class StringCalculator
+  def self.calculate(string_numbers)
+    # Forma Pro en HD
+    string_numbers.split(",").map(&:to_i).sum
+
+    # Forma Pro
+    # - - - - - - - - -
+    # return string_numbers.to_i if string_numbers.length < 2
+
+    # string_numbers.split(",").map(&:to_i).sum
+
+    # Forma Guarra
+    # - - - - - - - - -
+    # if(string_numbers.empty?)
+    #   0
+    # elsif(string_numbers.length==1)
+    #   string_numbers.to_i
+    # end
+    # total=0
+    # string_numbers.split(",").each { |number|
+    #   total += number.to_i
+    # }
+    # total
   end
 end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,6 +1,21 @@
 require './implementation'
-describe 'A test suite' do
-  it 'failing test' do
-    expect(Kata.example).to be_truthy
+describe 'StringCalculator' do
+  context 'implements calculate method' do
+
+    it 'with empty value as input returns 0' do
+      expect(StringCalculator.calculate("")).to eq 0
+    end
+
+    it 'with only one string value as input returns same number' do
+      expect(StringCalculator.calculate("1")).to eq 1
+    end
+
+    it 'with two strings values as input returns total sum' do
+      expect(StringCalculator.calculate("2,3")).to eq 5
+    end
+
+    it 'with more than strings values as input returns total sum' do
+      expect(StringCalculator.calculate("1,2,3,4,5,6,7,8,9")).to eq 45
+    end
   end
 end
